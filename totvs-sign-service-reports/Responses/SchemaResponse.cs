5392fb811541d7fa802e719f2601bcfc231771ee
Ronan Vale de Oliveira <ronan.oliveira@totvs.com.br>
2023-03-02 15:37:21 -0300

    Implementação da rota Discovery

---
+++ totvs-sign-service-reports/Responses/SchemaResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace totvs_sign_service_reports.Responses
+{
+    public class SchemaResponse
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public List<string> Areas { get; set; }
+        public string SchemaUrl { get; set; }
+        public string DataUrl { get; set; }
+
+        public List<PropertiesSchema> Properties { get; set; }
+        public List<ParametersSchema> Parameters { get; set; }
+    }
+
+    public class PropertiesSchema
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public FilterPropertie Filter { get; set; }
+    }
+
+    public class FilterPropertie
+    {
+        public string OptionsUrl { get; set; }
+        public bool IsRequired { get; set; }
+    }
+
+    public class ParametersSchema
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Type { get; set; }
+        public bool MultiValue { get; set; }
+    }
+}
